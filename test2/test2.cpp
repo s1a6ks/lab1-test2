5ed702bd93a8618f5ee7f9f31447995eed83492b
Макс Горецький <s1a6ks@gmail.com>
2023-09-13 08:46:29 +0300

    test 2

---
+++ test2/test2.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+    double weightInPounds = 15, weightInKilograms;
+    
+    weightInKilograms = weightInPounds * 0.45359237;
+
+    printf("Weight in kilograms: %.2lf\n", weightInKilograms);
+
+    return 0;
+}
